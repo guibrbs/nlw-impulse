c4a5c1e2124754323bd942301c0c4820a73c08ad
guibrbs <200037994@aluno.unb.br>
2022-05-08 02:11:10 -0300

    :rocket: Increasing the code

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,20 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm(){
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)]  md:w-auto">
-            Hello World
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+                
+            </div>
+
+            <footer>
+                Feito com 🤍 pelo <a className="underline underline-offset-2" href="https://guibrbs.github.io/Portfolio" target={"_blank"}> Guilherme</a>
+            </footer>
         </div>
     )
 }
