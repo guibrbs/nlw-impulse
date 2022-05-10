84e028b79f64f3ebde33eb05fc0d29c518296eb7
guibrbs <200037994@aluno.unb.br>
2022-05-10 11:49:42 -0300

    :rocket: Setting up onclick new feedback

--- src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,6 +1,10 @@
 import { CloseButton } from "../../CloseButton";
 
-export function FeedbackSuccessStep(){
+interface FeedbackSuccessStepProps{
+    onFeedbackRestartRequested: () => void
+}
+
+export function FeedbackSuccessStep( { onFeedbackRestartRequested }: FeedbackSuccessStepProps ){
     return(
         <>
             <header>
@@ -17,6 +21,7 @@ export function FeedbackSuccessStep(){
                     type="button"
                     className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors 
                     focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500"
+                    onClick={onFeedbackRestartRequested}
                 >
                     Quero enviar outro
                 </button>
